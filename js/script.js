8f2a1dd2c2eb12fed6aceb663022085a2eaf7dc2
Tom Sabbe <tom.sabbe13@gmail.com>
2020-12-21 16:20:11 +0100

    add datafile and basic script

--- js/script.js
+++ js/script.js
@@ -3,27 +3,20 @@
 window.addEventListener('load', Initieer);
 
 //global variables
-var rdbAll;
-var black;
+var slcHouses;
+var eyesFilter;
 
 
 function Initieer() {
     slcHouses = document.querySelector('#slcHouses');
-    slcAncestry = document.querySelector('#slcAncestry');
-    placeholderCards = document.querySelector('#placeholderCards');
-    hairFilter = document.querySelector('#hairFilter');
     eyesFilter = document.querySelector('#eyesFilter');
 
     slcHouses.addEventListener("change", SelectedHouse);
-    slcAncestry.addEventListener("change", SelectedAncestry);
 
     Start();
 }
 
 function Start() {
     AddHairRadioButton();
-    AddEyesRadioButton();
-    ShowAllCards();
     FillHouses();
-    FillAncestry();
 }
