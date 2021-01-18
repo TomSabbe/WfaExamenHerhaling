5fd41237ca3d6942bdf08717d6c629feb3b8fcba
Tom Sabbe <tom.sabbe13@gmail.com>
2021-01-18 19:55:50 +0100

    img tryuit

--- js/script.js
+++ js/script.js
@@ -2,75 +2,84 @@
 
 window.addEventListener('load', Initieer);
 
-//global variables
-var slcPlatform;
-var divGekozenPlatform;
-var gekozenPlatform = null;
-var secDetails, secBestelling;
-
-var platfrom;
+//globals
+//index links
+var slcplatformKeuze;
+var h3titelPlatform;
+var divafbeeldingenGamesVoorPlatforms;
+//global vars
+var gamesCollection;
+var ChoiceGamePlatform;
 
 function Initieer() {
-    //koppel elementen
+    //console.log(`testing area`);
     KoppelDomElementen();
-    //event listners
     VoegEventListenersToe();
-
-    // Functieuitvoer bij start
-    Start();
-
-
+    FetchData();
 }
 
-function Start() {
-    console.log(`testing area`);
-    OnlineFetch();
-    
-}
-
-// Functies aanroepen 
 
 function KoppelDomElementen() {
-    slcPlatform = document.getElementById("platform");
-    divGekozenPlatform = document.getElementById("gekozenPlatform");
-    secDetails = document.getElementById("details");
-    secBestelling = document.getElementById("bestelling");
+    slcplatformKeuze = document.getElementById("platformKeuze");
+    h3titelPlatform = document.getElementById("titelPlatform");
+    divafbeeldingenGamesVoorPlatforms = document.getElementById("afbeeldingenGamesVoorPlatforms");
 }
 
 function VoegEventListenersToe() {
-    slcPlatform.addEventListener('change', ToonGekozenPlatform);
-    
+    slcplatformKeuze.addEventListener('change', ToonGekozenPlatformImg);
+
 }
 
-function OnlineFetch() {
-          //Fetch online
-          fetch('https://tomsabbe.github.io/WfaExamenHerhaling/api/games.json')
-          .then(function (resp) { return resp.json(); })
-          .then(function (arr) {
-            ToonPlatforms(arr);
-
-          });
+function FetchData() {
+    fetch('https://tomsabbe.github.io/WfaExamenHerhaling/api/games.json')
+        .then(function (resp) { return resp.json(); })
+        .then(function (data) {
+            gamesCollection = data;
+            VulSelect(data);
+        })
+        .catch(function(err) { 
+            divafbeeldingenGamesVoorPlatforms.innerHTML = err;
+            console.log(err);
+         });
+        
 }
 
-
-
-function ToonPlatforms() {
-    slcPlatform.options.lenght = 0;
-
+// Functies aanroepen 
+function VulSelect(games) {
     for (let plat in games) {
-        slcPlatform.options[slcPlatform.length] = new Option(plat, plat);
+        let op = document.createElement("option");
+        op.textContent = plat;
+        op.value = plat;
+        slcplatformKeuze.appendChild(op);
+        console.log(plat);
     }
+    slcplatformKeuze.selectedIndex= 1;
+    ToonGekozenPlatformImg();
+}
 
-    ToonGekozenPlatform();
+function ToonGekozenPlatformImg() {
+    let index = slcplatformKeuze.value;
+    ChoiceGamePlatform = gamesCollection[index];
+    titelPlatform.innerHTML = index;
+
+    ChoiceGamePlatform.forEach(picture => {
+        let imgGame = document.createElement("img");
+        imgGame.src = picture.Image;
+        imgGame.className = "imgGame";
+        console.log(imgGame);
+        divafbeeldingenGamesVoorPlatforms.appendChild(imgGame);
+    });
+
 }
 
 
-function ToonGekozenPlatform() {
-    gekozenPlatform = slcPlatform.selectedIndex;
 
-    let x = document.createElement('p');
-    x.innerHTML = "gekozen platform";
-    divGekozenPlatform.appendChild(x);
 
+
+
+
+
+function ResetFields() {
+    divafbeeldingenGamesVoorPlatforms.innerHTML = "-";
+    txtBirthYear.value = "";
 }
-
