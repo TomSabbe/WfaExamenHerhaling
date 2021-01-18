d3e9c9b5ece9027e4e6188a9cebc1c15cb237d6d
Tom Sabbe <tom.sabbe13@gmail.com>
2021-01-18 21:49:40 +0100

    Click img

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ window.addEventListener('load', Initieer);
 var slcplatformKeuze;
 var h3titelPlatform;
 var divafbeeldingenGamesVoorPlatforms;
+var details;
 //global vars
 var gamesCollection;
 var ChoiceGamePlatform;
@@ -22,6 +23,7 @@ function KoppelDomElementen() {
     slcplatformKeuze = document.getElementById("platformKeuze");
     h3titelPlatform = document.getElementById("titelPlatform");
     divafbeeldingenGamesVoorPlatforms = document.getElementById("afbeeldingenGamesVoorPlatforms");
+    details = document.getElementById("details");
 }
 
 function VoegEventListenersToe() {
@@ -68,71 +70,48 @@ function ToonGekozenPlatformImg() {
         imgGame.className = "imgGame";
         //imgGame.setAttribute('onclick','ToonDetails(picture.Name);');
         //or
-        imgGame.onclick= function() {ToonDetails(picture.Name);};
+        imgGame.onclick= function() {ToonDetails(picture);};
        
         divafbeeldingenGamesVoorPlatforms.appendChild(imgGame);
     });
 }
 
 function ToonDetails(game) {
+    ResetFields();
+    
     console.log(game);
-
 
     let form = document.createElement("form");
     form.className = "form";
+
     //naam
     let name = document.createElement("h2");
-    name.innerHTML = element.name;
+    name.innerHTML = game.Name;
     //type
-
+    let type = document.createElement("p");
+    type.innerHTML = game.Type;
     //prijs
-
-    //beschrijving
-
-    //detail foto's
-
-   
-
-    //merk logo
-    let imgMerk = document.createElement("img");
-    imgMerk.src = element.imgmerk;
-    imgMerk.className = "imgMerk";
-    //naam auto
-  
-    //foto auto
-    let imgCard = document.createElement("img");
-    imgCard.src = element.image;
-    imgCard.className = "imgCar";
-    //merk
-    let merk = document.createElement("p");
-    merk.innerHTML = element.merk;
-    //prijs €
     let price = document.createElement("p");
-    price.innerHTML = ` ${element.price} €`;
-    //koffer
-    let koffer = document.createElement("p");
-    koffer.innerHTML = `koffer volume ${element.koffervolume} L`;
+    price.innerHTML = game.Price;
+    //beschrijving
+    let beschrijving = document.createElement("p");
+    beschrijving.innerHTML = game.Description;
+    //detail foto's
+    /*
+    game.DetailImages.forEach(foto => {
+        let imgdetail = document.createElement("img");
+        imgdetail.src = foto.imgmerk;
+        imgdetail.className = "imgMerkDetail";
+   
+*/
+    
+    details.appendChild(form);
 
-    //a plus buy
-    let buybtn = document.createElement("a");
-    //img buy
-    let imgbuy = document.createElement("img");
-    imgbuy.src = element.image;
-    imgbuy.className = "imgCar";
-
-    document.getElementById("containerRoad").appendChild(article);
-
-    //uitvoering // !!!!!!! veranderen omdat extra array niet vergeten
-    let voering = document.createElement("p");
-    voering.innerHTML = element.Uitvoering;
-
-    article.appendChild(imgMerk);
-    article.appendChild(name);
-    article.appendChild(imgCard);
-    article.appendChild(merk);
-    article.appendChild(price);
-    article.appendChild(koffer);
-    article.appendChild(voering);
+    details.appendChild(name);
+    details.appendChild(type);
+    details.appendChild(price);
+    details.appendChild(beschrijving);
+    //details.appendChild(DetailImages);
 }
 
 
@@ -142,4 +121,5 @@ function ToonDetails(game) {
 
 function ResetFields() {
     divafbeeldingenGamesVoorPlatforms.innerHTML = "";
+    details.innerHTML = "";
 }
