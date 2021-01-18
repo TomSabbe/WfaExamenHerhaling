50a36aaf6445a297b148cb9a8c94719f99aa8bc4
Tom Sabbe <tom.sabbe13@gmail.com>
2021-01-18 21:42:11 +0100

    Voorlopig

--- js/script.js
+++ js/script.js
@@ -66,20 +66,73 @@ function ToonGekozenPlatformImg() {
         let imgGame = document.createElement("img");
         imgGame.src = picture.Image;
         imgGame.className = "imgGame";
-        //imgGame.setAttribute('onclick','ToonDetails();');
+        //imgGame.setAttribute('onclick','ToonDetails(picture.Name);');
         //or
         imgGame.onclick= function() {ToonDetails(picture.Name);};
        
         divafbeeldingenGamesVoorPlatforms.appendChild(imgGame);
-
-       
-
     });
 }
 
 function ToonDetails(game) {
     console.log(game);
-    //img.style.border ='5px solid pink';
+
+
+    let form = document.createElement("form");
+    form.className = "form";
+    //naam
+    let name = document.createElement("h2");
+    name.innerHTML = element.name;
+    //type
+
+    //prijs
+
+    //beschrijving
+
+    //detail foto's
+
+   
+
+    //merk logo
+    let imgMerk = document.createElement("img");
+    imgMerk.src = element.imgmerk;
+    imgMerk.className = "imgMerk";
+    //naam auto
+  
+    //foto auto
+    let imgCard = document.createElement("img");
+    imgCard.src = element.image;
+    imgCard.className = "imgCar";
+    //merk
+    let merk = document.createElement("p");
+    merk.innerHTML = element.merk;
+    //prijs €
+    let price = document.createElement("p");
+    price.innerHTML = ` ${element.price} €`;
+    //koffer
+    let koffer = document.createElement("p");
+    koffer.innerHTML = `koffer volume ${element.koffervolume} L`;
+
+    //a plus buy
+    let buybtn = document.createElement("a");
+    //img buy
+    let imgbuy = document.createElement("img");
+    imgbuy.src = element.image;
+    imgbuy.className = "imgCar";
+
+    document.getElementById("containerRoad").appendChild(article);
+
+    //uitvoering // !!!!!!! veranderen omdat extra array niet vergeten
+    let voering = document.createElement("p");
+    voering.innerHTML = element.Uitvoering;
+
+    article.appendChild(imgMerk);
+    article.appendChild(name);
+    article.appendChild(imgCard);
+    article.appendChild(merk);
+    article.appendChild(price);
+    article.appendChild(koffer);
+    article.appendChild(voering);
 }
 
 
